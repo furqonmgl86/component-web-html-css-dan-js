b7bf1a47f1d75d180eafafd12ed5a238bc1230b5
furqonmgl86 <fur_qon9462@yahoo.co.id>
2023-03-15 11:44:13 +0700

    Belajar Lagi

--- 3.Image-Gallery/js/script.js
+++ 3.Image-Gallery/js/script.js
@@ -1,6 +1,38 @@
+// function gantiGambar(gambarThumbnail) {
+//   let fullImg = document.getElementById("gambarBesar");
+//   fullImg.src = gambarThumbnail.src;
+//   // gambarThumbnail.style.opacity.toggle = "1";
+//   // gambarThumbnail.classList.toggle("mystyle");
+
+//   if (fullImg.src == gambarThumbnail.src) {
+//     console.log("oke");
+//     gambarThumbnail.classList.add("mystyle");
+//   }
+// }
+
+let fullImg = document.getElementById("gambarBesar");
+let imgThumbs = document.querySelectorAll(".thumb");
+console.log(imgThumbs);
+
 function gantiGambar(gambarThumbnail) {
-  let fullImg = document.getElementById("gambarBesar");
   fullImg.src = gambarThumbnail.src;
-  // gambarThumbnail.style.opacity.toggle = "1";
-  gambarThumbnail.classList.toggle("mystyle");
+
+  imgThumbs.forEach(function (thumb) {
+    thumb.style.opacity = "0.6";
+  });
+
+  gambarThumbnail.style.opacity = "1";
 }
+
+// let fullImg = document.getElementById("gambarBesar");
+// let imgThumbs = document.querySelectorAll(".thumb");
+// console.log(imgThumbs);
+
+// function gantiGambar(gambarThumbnail) {
+//   fullImg.src = gambarThumbnail.src;
+
+//   imgThumbs.forEach(function (thumb) {
+//     thumb.className = "thumbkan";
+//   });
+//   gambarThumbnail.classList.toggle("redupkan");
+// }
